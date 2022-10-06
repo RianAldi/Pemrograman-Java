6a1d482468b4a4042d15ea952b0316552ca5f900
Rian Aldi <114709152+RianAldi@users.noreply.github.com>
2022-10-06 18:33:44 +0700

    Pertemuan1

---
+++ Pertemuan1.java
@@ -0,0 +1,11 @@
+package PemrogramanBerbasisObjct;
+
+public class Pertemuan1 {
+    public static void main(String[] args) {
+        System.out.println("hello teman-teman");
+        System.out.println("Baris Ke-2 juga akang pukis");
+        System.out.println("Baris ke-3: ini adalah baris ketiga");
+        System.out.println("Baris Ke-6 :hallo juga hibloh");
+    }
+
+}
