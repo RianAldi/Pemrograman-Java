a418926145e18bb7f2ccec2fd6d4582383357ef4
Rian Aldi <114709152+RianAldi@users.noreply.github.com>
2022-10-06 18:36:22 +0700

    Pengenalan Print Dan Alur Eksekusi

--- Pertemuan1.java
+++ Pertemuan1.java
@@ -6,6 +6,6 @@ public class Pertemuan1 {
         System.out.println("Baris Ke-2 juga akang pukis");
         System.out.println("Baris ke-3: ini adalah baris ketiga");
         System.out.println("Baris Ke-6 :hallo juga hibloh");
-    }
+     }
 
 }
