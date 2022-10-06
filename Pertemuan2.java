4233dcdb96de27d59776456342e9c00c56db9c46
Rian Aldi <114709152+RianAldi@users.noreply.github.com>
2022-10-06 18:34:12 +0700

    Add files via upload

---
+++ Pertemuan2.java
@@ -0,0 +1,12 @@
+package PemrogramanBerbasisObjct;
+
+public class Pertemuan2 {
+    public static void main(String[] args) {
+        //ini variabel
+        int x = 10;
+        System.out.println("Nilai x adalah ="+ x);
+
+        x = 20;
+        System.out.println("Nilai x  baru adalah =" + x);
+    }
+}
